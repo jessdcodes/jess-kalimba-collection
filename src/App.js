1753e90d7f1da361af1467097d274a833e45e221
Jessica D <dionisiojessica15@gmail.com>
2025-10-28 13:29:25 +0800

    Initial App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        src="https://www.tabwhale.com/embed/cmh9wr03m0009h522tdwfqgic/ce77a3fdfaa97cef426a82b773f96e1c?instrument=kalimba&viewer=tab&note-notation=letters&accidentals-notation=sharp"
+        title="A Walk - Yerin Baek"
+        frameborder="0"
+        fetchpriority="low"
+        width="400"
+        height="400"
+      ></iframe>
     </div>
   );
 }
